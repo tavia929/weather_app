4e058e8a2e0000c22f6c892c566b2872aeb00b61
tavia929 <86624687+tavia929@users.noreply.github.com>
2021-08-28 14:51:07 -0400

    added API

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 function showTemperature(response) {
-  console.log(response.data);
-  let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = `${temperature}°C`;
-  let description = document.querySelector("#temperature-description");
-  description.innerHTML = response.data.weather[0].description;
-  }
-  
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "856b5bb0ab2beee3a272dcebd03929e8";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}=${apiKey}&units=metric`;
 
+temperatureElement.innerHTML = `${temperature}°C`;
+let description = document.querySelector("#temperature-description");
 axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 
 let city = "London";
