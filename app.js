04e067eabf4e9640a46dc7c8bc11813dd7a6f308
tavia929 <86624687+tavia929@users.noreply.github.com>
2021-08-28 15:04:18 -0400

    update date and time

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+function formatDate(timestamp) {
+  return "Friday";
+}
+
 function showTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "856b5bb0ab2beee3a272dcebd03929e8";
