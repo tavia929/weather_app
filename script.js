bfbea90f0335aaabb70c6375c2907348a16680b0
tavia929 <86624687+tavia929@users.noreply.github.com>
2021-08-30 10:08:50 -0400

    UI editing

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 function formatDate(date) {
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-  
-    let dayIndex = date.getDay();
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    let day = days[dayIndex];
-  
-    return `${day} ${hours}:${minutes}`;
+  let dayIndex = date.getDay();
 
+    let day = days[dayIndex];
+    let date = new Date(timestamp);
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let year = now.getFullYear();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[now.getDay()];
+
+    return `${month} ${day} ${date} ${year} ${hours}:${minutes}`;
     
   }
   
